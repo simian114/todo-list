d7850527b4a03647664d4dd66135118c12a9932f
sanam <simian114@gmail.com>
2021-08-30 17:09:17 +0900

    [#1] feat: styled-components 설치 및 설정

---
+++ src/assets/styles/GlobalStyles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+button {
+  cursor: pointer;
+  outline: none;
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`;
+
+export default GlobalStyles;
