0f343f9b4e6c9e0e680bd6d839f7a103be91c8e9
sanam <simian114@gmail.com>
2021-09-08 12:51:42 +0900

    [#39] feat: detailModal에 체크리스트 변경기능

---
+++ src/components/DetailTodoModal/CheckList/CheckList.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Form, Button, Checkbox, Input } from 'antd';
+import { Todo, CheckList as ICheckList } from 'service/redux/slices/todosSlice';
+import styled from 'styled-components';
+
+interface CheckListProps {
+  todo: Todo;
+  isEdit: boolean;
+  handleEditToggle: () => void;
+  setTodo: (val: any) => void;
+}
+const CheckList: React.FC<CheckListProps> = ({
+  todo,
+  isEdit,
+  handleEditToggle,
+  setTodo,
+}) => {
+  const makeNewCheckList = () => todo.checkList.map((item) => ({ ...item }));
+  const setTodoCheckList = (checkList: ICheckList) => {
+    setTodo({
+      ...todo,
+      checkList,
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newCheckList = makeNewCheckList();
+    newCheckList[+e.target.name].content = e.target.value;
+    setTodoCheckList(newCheckList);
+  };
+  const handleCheckboxToggle = (e: any) => {
+    const newCheckList = makeNewCheckList();
+    newCheckList[+e.target.name].checked = e.target.checked;
+    setTodoCheckList(newCheckList);
+  };
+  const handleAddCheckbox = () => {
+    const newCheckList = makeNewCheckList();
+    newCheckList.push({ checked: false, content: '' });
+    setTodoCheckList(newCheckList);
+  };
+  const handleDeleteCheckbox = (index: number) => {
+    const newCheckList = makeNewCheckList();
+    newCheckList.splice(index, 1);
+    setTodoCheckList(newCheckList);
+  };
+
+  return (
+    <div>
+      <h2>체크리스트</h2>
+      {todo.checkList.map((check, index) => (
+        <StyledFormItem key={index}>
+          <StyledCheckBox
+            hidden={isEdit}
+            checked={check.checked}
+            name={`${index}`}
+            onChange={handleCheckboxToggle}
+          >
+            {check.content || '내용을 넣어주세요 :)'}
+          </StyledCheckBox>
+          <StyledCheckboxContainer>
+            <StyledCheckboxInput
+              name={`${index}`}
+              hidden={!isEdit}
+              value={check.content}
+              onChange={handleChange}
+              placeholder="내용을 넣어주세요 :)"
+            />
+            <StyledButton
+              hidden={!isEdit}
+              onClick={() => handleDeleteCheckbox(index)}
+            >
+              삭제
+            </StyledButton>
+          </StyledCheckboxContainer>
+        </StyledFormItem>
+      ))}
+      <StyledButton hidden={isEdit} size="small" onClick={handleEditToggle}>
+        수정
+      </StyledButton>
+      <StyledButton hidden={!isEdit} size="middle" onClick={handleEditToggle}>
+        닫기
+      </StyledButton>
+      <StyledButton hidden={!isEdit} size="middle" onClick={handleAddCheckbox}>
+        추가
+      </StyledButton>
+    </div>
+  );
+};
+
+export default CheckList;
+
+const StyledFormItem = styled(Form.Item)`
+  margin-bottom: 0;
+`;
+
+const StyledButton = styled(Button)<{ hidden?: boolean }>`
+  ${({ hidden }) => hidden && `display: none;`}
+`;
+
+const StyledCheckBox = styled(Checkbox)<{ hidden?: boolean }>`
+  ${({ hidden }) => hidden && `display: none;`}
+  margin-bottom: 0;
+`;
+
+const StyledCheckboxContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledCheckboxInput = styled(Input)<{ hidden?: boolean }>`
+  ${({ hidden }) => hidden && `display: none;`}
+`;
--- src/components/DetailTodoModal/DetailTodoModal.tsx
+++ src/components/DetailTodoModal/DetailTodoModal.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Modal, Checkbox } from 'antd';
-import { Todo } from 'service/redux/slices/todosSlice';
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import { Todo, updateTodoRequest } from 'service/redux/slices/todosSlice';
 import styled from 'styled-components';
 import { getDate } from 'utils';
+import CheckList from './CheckList/CheckList';
+import { useDispatch } from 'react-redux';
 
 interface DetailTodoModalProps {
   visible: boolean;
@@ -13,19 +15,27 @@ interface DetailTodoModalProps {
 
 const DetailTodoModal: React.FC<DetailTodoModalProps> = ({
   visible,
-  todo,
+  todo: todoProp,
   closeModal,
   edit,
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [todo, setTodo] = useState({ ...todoProp });
+  const dispatch = useDispatch();
+
+  const updateCheckList = () => {
+    const updateTodo = { ...todo };
+    dispatch(updateTodoRequest({ updateTodo }));
+    closeModal();
+  };
+
   return (
     <Modal
       visible={visible}
-      okText="종료"
-      onOk={closeModal}
-      cancelText="수정"
-      onCancel={edit}
-      closable={false}
-      maskClosable={false}
+      okText="적용"
+      onOk={updateCheckList}
+      cancelText="닫기"
+      onCancel={closeModal}
     >
       <StyledTitle>{todo.title}</StyledTitle>
       {!!todo.description && (
@@ -34,22 +44,12 @@ const DetailTodoModal: React.FC<DetailTodoModalProps> = ({
           <br />
         </>
       )}
-      <StyledCheckList>
-        {/* TODO: 체크박스와 아이템 */}
-        체크리스트
-        <StyledListItem>
-          <Checkbox>text를 title로 수정</Checkbox>
-        </StyledListItem>
-        <StyledListItem>
-          <Checkbox>description 필드 생성</Checkbox>
-        </StyledListItem>
-        <StyledListItem>
-          <Checkbox>detail modal 컴포넌트 생성</Checkbox>
-        </StyledListItem>
-        <StyledListItem>
-          <Checkbox>checklist 필드 생성</Checkbox>
-        </StyledListItem>
-      </StyledCheckList>
+      <CheckList
+        todo={todo}
+        isEdit={isEdit}
+        handleEditToggle={() => setIsEdit(!isEdit)}
+        setTodo={setTodo}
+      />
       <br />
       <StyledDate>
         생성 날짜: {getDate(todo.createdAt)}
@@ -76,17 +76,4 @@ const StyledDescription = styled.p`
   padding-bottom: 10px;
   border-bottom: 1px solid #eeeeee;
   font-size: 16px;
-`;
-
-const StyledCheckList = styled.ul`
-  font-weight: 600px;
-  font-size: 16px;
-  font-style: italic;
-  padding-bottom: 10px;
-  border-bottom: 1px solid #eeeeee;
-`;
-
-const StyledListItem = styled.li`
-  font-size: 14px;
-  font-style: normal;
 `;
