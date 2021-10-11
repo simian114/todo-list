b52c54c24537abceb0e0b1e45dac5f069de640fe
sanam <simian114@gmail.com>
2021-10-11 18:44:30 +0900

    [#61] fix: 새로운 유저 접속시 컬렉션에 추가

--- src/service/firestore/userService.ts
+++ src/service/firestore/userService.ts
@@ -1,20 +1,13 @@
 import { db, functions } from 'firebase';
 import { getDoc } from 'firebase/firestore';
-import { doc, collection } from 'firebase/firestore';
-
-const usersRef = collection(db, 'users');
+import { doc, setDoc } from 'firebase/firestore';
 
 const Logger = functions.httpsCallable('userAuthLog');
 
 const getUser = async (uid: string) => {
-  const ref = doc(usersRef, uid);
-  const docRef = await getDoc(ref);
-
-  if (docRef.exists()) {
-    console.log('login!');
-    Logger({ type: 'login', user: uid });
-    return docRef.data();
-  } else return null;
+  const user = await getDoc(doc(db, `users/${uid}`));
+  if (user.exists()) return;
+  await setDoc(doc(db, 'users', uid), { user: uid, todos: [] });
 };
 
 const logoutUser = () => {
